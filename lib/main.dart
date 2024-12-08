a7fc61c2450dde77016cc483ec7e71e714ec9465
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-12-08 17:15:39 +0700

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/pages/bottomnav.dart';
 import 'package:food_app/pages/home.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const BottomNav(),
     );
   }
 }
--- lib/pages/bottomnav.dart
+++ lib/pages/bottomnav.dart
@@ -1,5 +1,9 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/pages/home.dart';
+import 'package:food_app/pages/order.dart';
+import 'package:food_app/pages/profile.dart';
+import 'package:food_app/pages/wallet.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -13,9 +17,51 @@ class _MyWidgetState extends State<BottomNav> {
   late List<Widget> pages;
   late Widget currentPage;
   late Home homepage;
-  late
+  late Profile profile;
+  late Order order;
+  late Wallet wallet;
+  @override
+  void initState() {
+    homepage = Home();
+    order = Order();
+    profile = Profile();
+    wallet = Wallet();
+    pages = [homepage, order, profile, wallet];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+          height: 65,
+          backgroundColor: Colors.white,
+          color: Colors.black,
+          animationDuration: Duration(milliseconds: 500),
+          onTap: (int index) {
+            setState(() {
+              currentTabIndex = index;
+            });
+          },
+          items: [
+            Icon(
+              Icons.home_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.wallet_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.person_outlined,
+              color: Colors.white,
+            ),
+          ]),
+      body: pages[currentTabIndex],
+    );
   }
 }
