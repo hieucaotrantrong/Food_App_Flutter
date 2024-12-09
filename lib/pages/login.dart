c8de5fd816bdbf59cf2a9a09d803b19217f4bd10
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-12-09 12:43:51 +0700

    app

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/widget/widget_support.dart';
 
 class LogIn extends StatefulWidget {
   const LogIn({super.key});
@@ -12,11 +13,11 @@ class _LogInState extends State<LogIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
+        child: Stack(
           children: [
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2,
+              height: MediaQuery.of(context).size.height / 2.5,
               decoration: BoxDecoration(
                   gradient: LinearGradient(
                       begin: Alignment.topLeft,
@@ -26,6 +27,122 @@ class _LogInState extends State<LogIn> {
                     Color(0xFFe74b1a),
                   ])),
             ),
+            Container(
+              margin:
+                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  )),
+              child: Text(""),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 60,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      "images/logo.png",
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      //login
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            "Login",
+                            style: AppWidget.HeadlineTextFieldStyle(),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: AppWidget.semiBoldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.email_outlined),
+                            ),
+                          ),
+                          //sing in
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: AppWidget.semiBoldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.password_outlined),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              "Forgot Password?",
+                              style: AppWidget.HeadlineTextFieldStyle(),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 80,
+                          ),
+                          Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: Color(0Xffff5722),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "LOGIN",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
