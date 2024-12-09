e11e7524e019761b45c474b3b189db2150efa4b9
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-12-09 11:38:18 +0700

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/pages/bottomnav.dart';
 import 'package:food_app/pages/home.dart';
+import 'package:food_app/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BottomNav(),
+      home: const LogIn(),
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                    Color(0xFFff5c30),
+                    Color(0xFFe74b1a),
+                  ])),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
