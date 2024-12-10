d4fbc6fd09e6147a5835d0fdd964db9344db352b
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-12-10 19:08:54 +0700

    app

--- lib/pages/forgotpassword.dart
+++ lib/pages/forgotpassword.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/pages/signup.dart';
 
@@ -10,6 +11,31 @@ class Forgotpassword extends StatefulWidget {
 
 class _ForgotpasswordState extends State<Forgotpassword> {
   TextEditingController mailcontroller = new TextEditingController();
+  String email = "";
+  final _formkey = GlobalKey<FormState>();
+  //reset pw
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+        "Mật khẩu đã được gửi về Email ",
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      )));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-foud") {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "Không tìm thấy tài khoản",
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        )));
+      }
+    }
+  }
 
   // --------------------------------------
   @override
@@ -46,102 +72,113 @@ class _ForgotpasswordState extends State<Forgotpassword> {
             ),
             Expanded(
                 child: Form(
+                    key: _formkey,
                     child: Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: ListView(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white70,
-                        width: 2.0,
+                      padding: EdgeInsets.only(left: 10),
+                      child: ListView(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white70,
+                                width: 2.0,
+                              ),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: TextFormField(
+                              controller: mailcontroller,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Vui long nhap Email";
+                                }
+                                return null;
+                              },
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                hintStyle: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Colors.white70,
+                                  size: 30,
+                                ),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              if (_formkey.currentState!.validate()) {
+                                setState(() {
+                                  email = mailcontroller.text;
+                                });
+                                resetPassword();
+                              }
+                            },
+                            child: Container(
+                              width: 140,
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Gửi Email",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Bạn chưa có tài khoản?",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignUp()));
+                                },
+                                child: Text(
+                                  "Tạo tài khoản",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 184, 166, 6),
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
                       ),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: TextFormField(
-                      controller: mailcontroller,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Vui long nhap Email";
-                        }
-                        return null;
-                      },
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      decoration: InputDecoration(
-                        hintText: "Email",
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.person,
-                          color: Colors.white70,
-                          size: 30,
-                        ),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    width: 140,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Gửi Email",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Bạn chưa có tài khoản?",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SignUp()));
-                        },
-                        child: Text(
-                          "Tạo tài khoản",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 184, 166, 6),
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ))),
+                    ))),
           ],
         ),
       ),
